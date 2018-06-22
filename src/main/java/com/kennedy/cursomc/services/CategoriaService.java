cca5bc1a466a50d3c5ea49a12fcd4923ba233964
kennedyrocha <kennedy279972@gmail.com>
2018-06-22 16:46:30 -0400

    Criando repository e service para Categoria

---
+++ src/main/java/com/kennedy/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.kennedy.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kennedy.cursomc.domain.Categoria;
+import com.kennedy.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}
