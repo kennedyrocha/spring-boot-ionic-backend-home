f7bf127160454060f2c48ecfda3f7766abdc1d92
kennedyrocha <kennedy279972@gmail.com>
2018-06-28 12:56:55 -0400

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/kennedy/cursomc/services/PedidoService.java
@@ -0,0 +1,27 @@
+package com.kennedy.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kennedy.cursomc.domain.Pedido;
+import com.kennedy.cursomc.repositories.PedidoRepository;
+import com.kennedy.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		
+		Optional<Pedido> obj = repo.findById(id);
+
+		return obj.orElseThrow(() ->  new ObjectNotFoundException("Objeto não encontrado id: " + id
+												+ ", Tipo: " + Pedido.class.getName()));
+		
+	}
+
+}
