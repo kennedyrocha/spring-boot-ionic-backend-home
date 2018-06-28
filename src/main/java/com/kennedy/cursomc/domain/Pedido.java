038fad55adab028e8f50dc0f8f0126077040c571
kennedyrocha <kennedy279972@gmail.com>
2018-06-28 16:19:58 -0400

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/kennedy/cursomc/domain/Pedido.java
+++ src/main/java/com/kennedy/cursomc/domain/Pedido.java
@@ -23,27 +23,25 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 @Entity
 public class Pedido implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@JsonFormat(pattern="dd/MM/yyyy HH:mm")
 	private Date instante;
-	
-	@JsonManagedReference
+
 	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
-	
-	@JsonManagedReference
+
 	@ManyToOne
 	@JoinColumn(name="clinte_id")
 	private Cliente cliente;
-	
+
 	@ManyToOne
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
-	
+
 	@OneToMany(mappedBy="id.pedido")
 	private Set<ItemPedido> itens = new HashSet<>();
 	
