941d3f607560865fe2cefd2012416266650ac769
unknown <kennedy_snp@hotmail.com>
2018-07-18 11:49:05 -0400

    Detando uma Categoria com DETELE

--- src/main/java/com/kennedy/cursomc/services/CategoriaService.java
+++ src/main/java/com/kennedy/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.kennedy.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.kennedy.cursomc.domain.Categoria;
 import com.kennedy.cursomc.repositories.CategoriaRepository;
+import com.kennedy.cursomc.services.exceptions.DataIntegrityException;
 import com.kennedy.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -34,6 +36,19 @@ public class CategoriaService {
 		
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete (Integer id) {
+		
+		this.find(id);
+		
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			
+			throw new DataIntegrityException("Não possível excluir uma categoria que possui produtos");
+		}
 	}
 
 }
