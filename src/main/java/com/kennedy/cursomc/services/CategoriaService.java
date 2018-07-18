39151c436404bd0d0c9ee1f7eaadc762f3f5039a
unknown <kennedy_snp@hotmail.com>
2018-07-18 12:03:48 -0400

    Listando todas as categorias com DTO

--- src/main/java/com/kennedy/cursomc/services/CategoriaService.java
+++ src/main/java/com/kennedy/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.kennedy.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,6 +50,11 @@ public class CategoriaService {
 			
 			throw new DataIntegrityException("Não possível excluir uma categoria que possui produtos");
 		}
+	}
+	
+	public List<Categoria> findAll(){
+		
+		return repo.findAll();
 	}
 
 }
