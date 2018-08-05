2d1eff0e27fbf9596fe605883f6ce3d609ec6c70
unknown <kennedy_snp@hotmail.com>
2018-08-04 23:48:42 -0400

    Email HTML

--- src/main/java/com/kennedy/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/kennedy/cursomc/services/SmtpEmailService.java
@@ -1,15 +1,21 @@
 package com.kennedy.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstratcEmailService {
 	
 	@Autowired
 	private MailSender mailSender;
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
 	
 	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
 	
@@ -18,6 +24,14 @@ public class SmtpEmailService extends AbstratcEmailService {
 
 		LOG.info("Enviando envio de email...");
 		mailSender.send(msg);
+		LOG.info("Email Enviado");
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+
+		LOG.info("Enviando envio de email...");
+		javaMailSender.send(msg);
 		LOG.info("Email Enviado");
 	}
 
