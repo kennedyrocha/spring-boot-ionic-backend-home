342a6919162c3dfa1cba8fb37588a05c6521521f
unknown <kennedy_snp@hotmail.com>
2018-12-29 12:10:02 -0300

    Salvando a URL da imagem em Cliente

--- src/main/java/com/kennedy/cursomc/services/ClienteService.java
+++ src/main/java/com/kennedy/cursomc/services/ClienteService.java
@@ -129,7 +129,20 @@ public class ClienteService {
 	}
 	
 	public URI updaloadProfilePicture(MultipartFile multipartFile) {
-		return s3service.uploadFile(multipartFile);
+		
+		UserSS user = UserService.authenticated();
+		
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado!");
+		}
+		
+		URI uri = s3service.uploadFile(multipartFile);
+		
+		Optional<Cliente> cli = repo.findById(user.getId());		
+		cli.get().setImageUrl(uri.toString());
+		repo.save(cli.get());
+		
+		return uri;
 	}
 
 }
