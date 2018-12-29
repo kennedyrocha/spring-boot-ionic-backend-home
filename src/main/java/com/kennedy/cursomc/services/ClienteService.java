471c6432bf35ca7e45901cb4f530450919f7477c
unknown <kennedy_snp@hotmail.com>
2018-12-29 14:07:04 -0300

    Bônus: ajustando tamanho da imagem

--- src/main/java/com/kennedy/cursomc/services/ClienteService.java
+++ src/main/java/com/kennedy/cursomc/services/ClienteService.java
@@ -51,6 +51,9 @@ public class ClienteService {
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
 	
+	@Value("${img.profile.size}")
+	private Integer imageSize;
+	
 	public Cliente find(Integer id) {
 		
 		UserSS user = UserService.authenticated();
@@ -145,6 +148,8 @@ public class ClienteService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, imageSize);
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return s3service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
