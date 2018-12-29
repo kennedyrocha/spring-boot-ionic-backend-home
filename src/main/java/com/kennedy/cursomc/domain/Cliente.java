4036f6e460482a6f7e107ae07bfb007e271456c2
unknown <kennedy_snp@hotmail.com>
2018-12-29 13:48:02 -0300

    Usando padrão de nomes para imagens

--- src/main/java/com/kennedy/cursomc/domain/Cliente.java
+++ src/main/java/com/kennedy/cursomc/domain/Cliente.java
@@ -55,8 +55,6 @@ public class Cliente implements Serializable{
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 
-	private String imageUrl;
-	
 	private Cliente() {
 		this.addPerfil(Perfil.CLIENTE);
 	}
@@ -175,13 +173,5 @@ public class Cliente implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
 	}
 }
